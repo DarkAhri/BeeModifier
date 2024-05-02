141a2c8a58d2e9741975c028f493b5cd24291e79
DarkAhri <1132464027@qq.com>
2024-05-02 21:10:12 +0800

    SpotlessApply

--- src/main/java/com/DarkAhri/beebee/mixins/MixinQueenWorkTick.java
+++ src/main/java/com/DarkAhri/beebee/mixins/MixinQueenWorkTick.java
@@ -8,16 +8,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import forestry.api.apiculture.IBee;
 import forestry.apiculture.BeekeepingLogic;
 
-@Mixin(value = BeekeepingLogic.class,remap = false)
+@Mixin(value = BeekeepingLogic.class, remap = false)
 public class MixinQueenWorkTick {
 
     @Shadow
     private int queenWorkCycleThrottle;
 
-    @Inject(
-        method = "queenWorkTick",
-        at = @At("TAIL"),
-        remap = false)
+    @Inject(method = "queenWorkTick", at = @At("TAIL"), remap = false)
     private void queenWorkTick(IBee queen, CallbackInfo ci) {
         queenWorkCycleThrottle += 5;
     }
